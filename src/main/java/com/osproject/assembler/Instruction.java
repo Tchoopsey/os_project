a11c23cb94f73736826bffa49bec0e7a91c85b3b
emikova <ajzacebarabcino46@gmail.com>
2026-09-25 16:08:06 +0200

    Implementation of requirements from the table

---
+++ src/main/java/com/osproject/assembler/Instruction.java
@@ -0,0 +1,29 @@
+package com.osproject.assembler;
+
+import java.util.Locale;
+
+public class Instruction {
+    private String opcode;
+    private int operand;
+
+    public Instruction(String opcode, int operand){
+        this.opcode = opcode.toUpperCase();
+        this.operand = operand;
+    }
+
+    public String getOpcode(){
+        return opcode;
+    }
+
+    public int getOperand(){
+        return operand;
+    }
+
+    @Override
+    public String toString() {
+        return "Instruction{" +
+                "opcode='" + opcode + '\'' +
+                ", operand=" + operand +
+                '}';
+    }
+}
